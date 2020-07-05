0db6a7e22c64d760fa7e7aff4dc781f028e20149
Frida Eriksson <Frida.Eriksson@silf.se>
2020-07-05 15:44:17 +0200

    started creating user model

---
+++ backend/models/user.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 40
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /\S+@\S+\.\S+/
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  /* messages: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'message'
+  }, */
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,32 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/code-challenge1"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defines the port the app will run on. Defaults to 8080.
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/messages', async (req, res) => {
+  con
 })
 
 // Start the server
